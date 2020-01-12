29c266695aca32e88526726dd890e0127ad35211
jyll sarta <illstarred.dive@gmail.com>
2020-01-12 17:13:50 +0900

    ログウィンドウを書いた

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -31,6 +31,22 @@
         | ステータス
     .chat.window
     .log.window
+      .item
+        .at
+          | 11:22
+        .content
+          | ☆神殺しの彗星のアミュレット+65535を拾った！
+      .item
+        .at
+          | 11:22
+        .content
+          | ★食品サンプルを拾った！
+      .item
+        .at
+          | 11:22
+        .content
+          | 野うさぎ4体との戦闘だ！
+          | HP: (80,54)→(66,13) / 4ターン
 </template>
 
 <script lang="ts">
@@ -124,10 +140,34 @@ export default {
   }
   .log{
     height: 600px - 160px - $space * 4;
-    width: 250px;
+    width: 300px;
     top: $space * 2 + 60px;
     left: $space;
-    
+    display: flex;
+    flex-direction: column;
+    font-size: $font-size-mini;
+    .item{
+      width: 100%;
+      line-height: 120%;
+      display: flex;
+      margin-bottom: $thin_space;
+      .at{
+        width: 3rem; // "11:33 " ぶんのスペース
+        border-right: 1px solid $gray2;
+        padding-right: $thin_space;
+      }
+      .content{
+        display: inline-block;
+        width: 100%;
+        flex: 1;
+        white-space: pre-wrap;
+        padding-left: $thin_space;
+      }
+      &:hover{
+        filter: brightness(110%);
+      }
+    }
+
   }
   .chat{
     height: 100px;
