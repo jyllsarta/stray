3d7c9f7988e1bd6de10440fc5581f332ec5ec24d
jyll sarta <illstarred.dive@gmail.com>
2020-01-12 16:37:14 +0900

    右メニュー追加

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -23,6 +23,12 @@
         .content
           | 26442F
     .right_menu.window
+      .item
+        | 回復
+      .item
+        | 装備
+      .item
+        | ステータス
     .chat.window
     .log.window
 </template>
@@ -99,12 +105,29 @@ export default {
     width: 200px - $space * 3;
     top: $space;
     right: $space;
+    display: flex;
+    flex-direction: column;
+    font-size: $font-size-normal;
+    .item{
+      width: 100%;
+      height: 40px;
+      background-color: $main-color;
+      margin-bottom: $space;
+      border-radius: $radius;
+      line-height: 100%;
+      padding-top: (40px - $font-size-normal) / 2;
+      padding-left: $space;
+      &:hover{
+        filter: brightness(110%);
+      }
+    }
   }
   .log{
     height: 600px - 160px - $space * 4;
     width: 250px;
     top: $space * 2 + 60px;
     left: $space;
+    
   }
   .chat{
     height: 100px;
