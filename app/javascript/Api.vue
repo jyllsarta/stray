cc9055b3b80a4df90f127e59721ce567b121740f
jyll sarta <illstarred.dive@gmail.com>
2020-01-17 21:56:26 +0900

    認証系を実装(クライアント)

--- app/javascript/Api.vue
+++ app/javascript/Api.vue
@@ -16,8 +16,15 @@ export default {
     init(){
       console.log("mounted api system!");
       this.fetchMasterData();
-      this.fetchUserModel();
-      this.fetchLatestEvents();
+      this.loadUserData();
+    },
+    loadUserData(){
+      if(localStorage.access_token){
+        this.fetchUserModel();
+      }
+      else{
+        this.signUp();
+      }
     },
     fetchMasterData(){
       const path = `/masterdata.json`;
@@ -33,7 +40,7 @@ export default {
         });
     },
     fetchUserModel(){
-      const user_id = 333;
+      const user_id = localStorage.user_id;
       const path = `/users/${user_id}/status.json`;
       axios.get(path)
         .then((results) => {
@@ -47,13 +54,39 @@ export default {
         });
     },
     fetchLatestEvents(){
-      const user_id = 333;
+      const user_id = localStorage.user_id;
       const path = `/users/${user_id}/events.json`;
       axios.get(path)
         .then((results) => {
           console.log(results);
           console.log("OK");
           this.$store.commit("updateLatestEvents", results.data.events)
+        })
+        .catch((results) => {
+          console.warn(results);
+          console.warn("NG");
+        });
+    },
+    signUp(){
+      const path = `/users`;
+      axios.post(
+        path,
+        {
+          authenticity_token: document.querySelector("meta[name=csrf-token]").attributes["content"].textContent
+        },
+        {
+          headers: {
+            accept: 'application/json',
+            data: {},
+          }
+        })
+        .then((results) => {
+          console.log(results);
+          console.log("OK");
+          // TODO: LocalStorageもある主グローバル変数なので、変更させる場所を一元管理したほうがいいかもなあ
+          localStorage.user_id = results.data.user_id;
+          localStorage.access_token = results.data.access_token;
+          this.fetchUserModel();
         })
         .catch((results) => {
           console.warn(results);
