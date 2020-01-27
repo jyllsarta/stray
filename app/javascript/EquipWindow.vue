91eae586abd9a50623e9cc4fba206a88aae6eb65
jyllsarta <illstarred.dive@gmail.com>
2020-01-27 18:43:05 +0900

    ステータス画面の書き途中

--- app/javascript/EquipWindow.vue
+++ app/javascript/EquipWindow.vue
@@ -7,22 +7,73 @@
         .title
           | 装備
       .body
-        .chara
-          | Spica
         .sub_chara
-          | Tirol
-        .reinforcements
-          | Reinforcements
-        .sub_chara_equips
-          | Tirol
-        .sub_chara_status
-          | Status
-        .item_list_main
-          | Equips
-        .detail
-          | Detail
-        .main_chara_equips
-          | List
+          img.sub_character_image(src="images/ui/tirol.png")
+        .chara
+          img.character_image(src="images/ui/spica.png")
+        .reinforcements.block
+          .label
+            | 加護
+          .reinforcement_list
+            .reinforcement
+              | 先制Lv2
+            .reinforcement
+              | 雷光Lv4
+        .sub_chara_status.block
+          .label
+            | チロルのステータス
+          .status
+            .atk
+              | ATK: 351287952
+            .atk
+              | DEF: 659998714
+        .item_list_main.block
+          .label
+            | アイテム
+        .detail.block
+          .label
+            | 詳細
+        .main_chara_equips.block
+          .label
+            | スピカのステータス
+          .main
+            .equips
+              .equip
+                | ★やさしいなんでもない系装備+878763
+              .equip
+                | ★やさしいなんでもない系装備+878763
+              .equip
+                | ★やさしいなんでもない系装備+878763
+              .equip
+                | ★やさしいなんでもない系装備+878763
+            .current_parameters
+              .status
+                | STR 987654321
+              .status
+                | STR 987654321
+              .status
+                | STR 987654321
+              .status
+                | STR 987654321
+            .this_item
+              .status
+                | +987654321
+              .status
+                | +987654321
+              .status
+                | +987654321
+              .status
+                | +987654321
+            .to_status
+              .status
+                | ATK: 987654321
+              .status_diff
+                | (+987654321)
+              .status
+                | DEF: 987654321
+              .status_diff
+                | (+987654321)
+
 
 </template>
 
@@ -47,24 +98,91 @@ export default {
 <style lang="scss" scoped>
 @import "stylesheets/global_setting";
 
+// 通常のスタイル定義
+.body{
+  .block{
+    border-left: 1px solid $gray3;
+    //border-radius: $radius;
+  }
+
+  .label{
+    font-size: $font-size-large;
+    color: $accent-color;
+    line-height: 100%;
+  }
+  .character_image{
+    width: 220px;
+    transform: scale(-1,1);
+  }
+  .sub_character_image{
+    width: 160px;
+  }
+
+  .reinforcements{
+    .reinforcement_list{
+      display: flex;
+      flex-direction: column;
+      padding: $thin_space;
+      .reinforcement{
+        margin: $thin_space;
+        border-bottom: 1px solid $gray3;
+        width: 45%;
+        text-align: center;
+        &::before{
+          content: "◇";
+        }
+      }
+    }
+  }
+
+  .sub_chara_status{
+    font-size: $font-size-mini;
+  }
+
+  .main_chara_equips{
+    .main{
+      display: flex;
+      flex-direction: row;
+      height: calc(100% - #{$font-size-large});
+      .equips{
+        width: 45%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        .equip{
+          padding: 2px;
+          &:nth-child(1){
+            padding-left: 8px;
+          }
+          &:nth-child(2){
+            padding-left: 16px;
+          }
+          &:nth-child(3){
+            padding-left: 24px;
+          }
+          &:nth-child(4){
+            padding-left: 32px;
+          }
+        }
+      }
+    }
+  }
+}
+
+
+// 箱自体の配置
 // 頭がおかしくなりそうなんだけどこういうのの配置って通常どうなってるんです？
 
 $detail-width: 150px;
 $character-width: 200px;
-$character-height: 400px;
+$character-height: 320px;
 $sub-character-width: 100px;
-$sub-character-height: 200px;
+$sub-character-height: 170px;
 $main-chara-equip-height: 170px;
 $item_list-main-width:400px;
 $reinforcement-list-height: 200px;
-$sub-chara-status-height: 40px;
 
 .body{
-  * {
-    border: 1px solid $gray3;
-    font-size: $font-size-large;
-    color: $accent-color;
-  }
   .chara{
     position: absolute;
     bottom: $thin_space;
@@ -86,19 +204,12 @@ $sub-chara-status-height: 40px;
     width: calc(100% - #{$item_list-main-width} - #{$detail-width} - #{$character-width} - #{$thin_space * 3});
     height: $reinforcement-list-height;
   }
-  .sub_chara_equips{
-    position: absolute;
-    top: $reinforcement-list-height + $sub-chara-status-height + $thin_space * 3;
-    left: $character-width;
-    width: calc(100% - #{$item_list-main-width} - #{$detail-width} - #{$character-width} - #{$thin_space * 3});
-    height: calc(100% - #{$main-chara-equip-height} - #{$reinforcement-list-height} - #{$sub-chara-status-height} - #{$thin_space * 5});
-  }
   .sub_chara_status{
     position: absolute;
     top: $reinforcement-list-height + $thin_space * 2;
     left: $character-width;
     width: calc(100% - #{$item_list-main-width} - #{$detail-width} - #{$character-width} - #{$thin_space * 3});
-    height: $sub-chara-status-height;
+    height: calc(100% - #{$main-chara-equip-height} - #{$reinforcement-list-height} - #{$thin_space * 5});
   }
   .item_list_main{
     position: absolute;
