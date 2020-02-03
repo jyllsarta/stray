cebad18f72ecf1d3696c1a497fabd1de6ddf1aae
jyll sarta <illstarred.dive@gmail.com>
2020-02-04 00:13:44 +0900

    そういえばサーバに装備モデルもう作ってあったわ(そしてモックがレスポンス信頼だったのですぐ入った)

--- app/models/user/character.rb
+++ app/models/user/character.rb
@@ -2,4 +2,8 @@ class User::Character < ApplicationRecord
   belongs_to :user
   has_many :equips, foreign_key: :user_character_id
   enum character_id: { spica: 1, tirol: 2 }
+
+  def equip_item_ids
+    self.equips.map(&:user_item).map{|ui| ui&.item_id}
+  end
 end
--- app/models/user/character/equip.rb
+++ app/models/user/character/equip.rb
@@ -1,3 +1,4 @@
 class User::Character::Equip < ApplicationRecord
   belongs_to :user_character, class_name: "User::Character"
+  belongs_to :user_item, class_name: "User::Item"
 end
