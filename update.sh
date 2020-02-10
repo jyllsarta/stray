f1e6c48112f83e27dabc604f0476ee0e8f84e539
jyll sarta <illstarred.dive@gmail.com>
2020-02-10 17:15:07 +0900

    追従スクリプトを更新

--- update.sh
+++ update.sh
@@ -1,7 +1,18 @@
+#!/bin/zsh
 # for update
 
+echo "# update repository"
 git pull
+
+echo "# bundle install"
 bundle install
+
+echo "# yarn install"
 yarn install --check-files
+
+echo "# db migrate"
 bundle exec rails db:ridgepole:apply
+
+echo "# db seed"
 bundle exec rails db:seed
+
