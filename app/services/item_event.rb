4d4f9cd241da06be927b1741bcd0cd2ad965cf3a
jyll sarta <illstarred.dive@gmail.com>
2020-02-11 21:05:32 +0900

    アイテムの抽選ロジックを実装

--- app/services/item_event.rb
+++ app/services/item_event.rb
@@ -1,8 +1,9 @@
 class ItemEvent < Event
-  def initialize(rank=0, at=Time.now)
+  def initialize(rank=1, at=Time.now)
     @at = at
     @rank = rank
-    @item_id = lot_item_id!
+    @rarity = lot_item_rarity!
+    @item_id = lot_item!(@rank, @rarity).id
     @amount = lot_amount!
   end
 
@@ -36,13 +37,27 @@ class ItemEvent < Event
     Constants.default_event_interval_seconds
   end
 
-private
+#private
   def item
     @_item ||= Item.find(@item_id)
   end
 
-  def lot_item_id!
-    @rank + SecureRandom.rand(1..5)
+  def lot_item_rarity!
+    seed = SecureRandom.rand(1..Constants.event.item.weights.sum)
+    sum = 0
+    Constants.event.item.weights.each_with_index do |weight, i|
+      sum += weight
+      return i if seed <= sum
+    end
+    raise "no way!"
+  end
+
+  def lot_item!(rank, rarity)
+    sample = ::Item.where(id: rank..(rank + Constants.event.item.id_range - 1)).where(rarity: rarity).sample
+    return sample if sample.present?
+
+    # サンプルの中に存在しないレアリティがピックされちゃった場合、それより前のid群からピックする
+    ::Item.where("id < #{rank}").where(rarity: rarity).order(id: :desc).limit(1).first
   end
 
   def lot_amount!
