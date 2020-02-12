4be1732d53db52593951bc83a8cfa0cd38a90995
jyllsarta <illstarred.dive@gmail.com>
2020-02-12 18:47:13 +0900

    set_defaultのところが意図せず変わってた...

--- app/models/user/character.rb
+++ app/models/user/character.rb
@@ -13,7 +13,7 @@ class User::Character < ApplicationRecord
   belongs_to :user
   has_many :equips, foreign_key: :user_character_id
   enum character_id: { spica: 1, tirol: 2 }
-  before_create :before_create
+  before_create :set_default
 
   def set_default
     self.hp = Constants.character.initial_hp_max
