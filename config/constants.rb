39a38ed9a4e05bf1a7477af0de4068b98d3d01de
jyll sarta <illstarred.dive@gmail.com>
2020-02-21 22:08:01 +0900

    constants をenvによって出し分ける

--- config/constants.rb
+++ config/constants.rb
@@ -1,3 +1,3 @@
 # http://shepherdmaster.hateblo.jp/entry/2016/11/03/225957
-# いったんjsonを挟んでからロードすると入れ子が合ってもきれいにOpenStructとしてパースしてくれるらしい
-Constants = JSON.parse(YAML.load_file("config/constants.yml").to_json, object_class: OpenStruct).freeze
+# いったんjsonを挟んでからロードすると入れ子があってもきれいにOpenStructとしてパースしてくれるらしい
+Constants = JSON.parse(YAML.load(ERB.new(File.read("config/constants.yml.erb")).result).to_json, object_class: OpenStruct).freeze
