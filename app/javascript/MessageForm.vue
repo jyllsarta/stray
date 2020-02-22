3473b8f853843aff9564f9cd0a96037fb85dcebc
jyll sarta <illstarred.dive@gmail.com>
2020-02-22 20:16:31 +0900

    お便りバリデーションを受け取ってメッセージを表示

--- app/javascript/MessageForm.vue
+++ app/javascript/MessageForm.vue
@@ -10,22 +10,55 @@
       textarea.input(v-model="message", cols=100)
       .send.clickable(@click="sendMessage")
         | 送信
+    .status
+      | {{status}}
 </template>
 
 <script lang="ts">
 import Constants from "./packs/constants.ts";
 import store from './packs/store.ts'
+import axios from "axios";
 
 export default {
   data: function () {
     return {
       message: "",
+      status: ""
     };
   },
   store,
   methods: {
+    onSuccess(){
+      this.message = "";
+      this.status= "送信しました！";
+    },
+    onError(msg){
+      this.status = msg || "なんかエラーが出ました";
+    },
     sendMessage(){
-      console.log("sent!")
+      const path = `/messages`;
+      axios.post(
+        path,
+        {
+          authenticity_token: document.querySelector("meta[name=csrf-token]").attributes["content"].textContent,
+          message: this.message,
+        },
+        {
+          headers: {
+            accept: 'application/json',
+            "X-AccessToken": localStorage.access_token,
+          }
+        })
+        .then((results) => {
+          console.log(results);
+          console.log("OK");
+          this.onSuccess();
+        })
+        .catch((error) => {
+          console.warn(error.response);
+          console.warn("NG");
+          this.onError(error.response.data.message);
+        });
     },
   },
 }
