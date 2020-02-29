044ca7b256697c292fee4df96d0fe3499d7fa168
jyll sarta <illstarred.dive@gmail.com>
2020-02-29 20:45:39 +0900

    500kbまでは許してたも...

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,4 +3,8 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
 　mode: 'production',
+  performance: {
+    maxEntrypointSize: 500000,
+    maxAssetSize: 500000,
+  },
 })
