d7502124107b5a65d01a004c63c62c088cd64ef7
jyll sarta <illstarred.dive@gmail.com>
2020-03-03 23:46:34 +0900

    codecov の導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 SimpleCov.start
 
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
