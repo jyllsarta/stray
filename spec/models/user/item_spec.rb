064aa1263999f1d7bac671d7f06a309244c0a458
jyll sarta <illstarred.dive@gmail.com>
2020-03-06 00:13:20 +0900

    item specを追記

--- spec/models/user/item_spec.rb
+++ spec/models/user/item_spec.rb
@@ -11,4 +11,19 @@
 require 'rails_helper'
 
 RSpec.describe User::Item, type: :model do
+  describe "#parameter" do
+    let(:user){ create(:user) }
+    let!(:item){ create(:item, str: 100, dex: 100, def: 200, agi: 0) }
+    let!(:user_item){ create(:user_item, user: user, item: item, rank: 5) }
+
+    subject { user_item.parameter }
+    it "returns character's parameter" do
+      expect(subject).to eq({
+                                str: 150, # 100 * 1.5
+                                dex: 150, # 100 * 1.5
+                                def: 300, # 200 * 1.5
+                                agi: 0,   #   0 * 1.5
+                            })
+    end
+  end
 end
