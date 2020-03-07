44b3f42ae60345d29fea805c8a8aea83d1ec6cbc
jyllsarta <illstarred.dive@gmail.com>
2020-03-07 19:45:49 +0900

    masterdata_specを追記

--- spec/requests/masterdata_spec.rb
+++ spec/requests/masterdata_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Masterdata", type: :request do
+  describe "GET /masterdata" do
+    let(:do_get) { get masterdata_path + ".json" }
+    subject do
+      do_get
+      response
+    end
+    it 'succeeds' do
+      subject
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)).to match_json_expression(
+                                               {
+                                                   masterdata: {
+                                                       items: Hash,
+                                                       dungeons: Hash,
+                                                   }
+                                               }
+                                           )
+    end
+  end
 end
