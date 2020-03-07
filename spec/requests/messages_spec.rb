76795f4c6ab1c9f5584d6927ed0f560bb9fd8fca
jyllsarta <illstarred.dive@gmail.com>
2020-03-07 19:34:53 +0900

    messages specを追記

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,4 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe "Messages", type: :request do
+  describe "GET /messages" do
+    let(:do_get) { get messages_path }
+    subject do
+      do_get
+      response
+    end
+    it 'succeeds' do
+      subject
+      expect(response).to have_http_status(200)
+      # HTML なのでレスポンス形式はテストしない
+    end
+  end
+
+  describe "POST /messages" do
+    include_context("stub_current_user")
+    let(:user){ create(:user) }
+    let(:do_post) { post messages_path, params: params }
+    subject do
+      do_post
+      response
+    end
+    context "succeeds" do
+      let(:params) do
+        {
+            message: "test message"
+        }
+      end
+      it 'succeeds' do
+        expect(subject).to have_http_status(200)
+        expect(JSON.parse(response.body)).to match_json_expression(
+                                                 {
+                                                     success: Boolean
+                                                 }
+                                             )
+      end
+    end
+    context "message was empty" do
+      let(:params) do
+        {
+            message: ""
+        }
+      end
+      it 'fails' do
+        expect(subject).to have_http_status(400)
+      end
+    end
+  end
+
 end
