ff1530bf31f000ea31559aff3593b54b11d364aa
jyllsarta <illstarred.dive@gmail.com>
2020-03-07 22:58:20 +0900

    serviceのspecを書き始めた

---
+++ spec/services/event_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  let(:event){ Event.new }
+  describe "#type" do
+    subject { event.type }
+    it "raises" do
+      expect{subject}.to raise_error NotImplementedError
+    end
+  end
+  describe "#detail" do
+    subject { event.detail }
+    it "raises" do
+      expect{subject}.to raise_error NotImplementedError
+    end
+  end
+  describe "#logs" do
+    subject { event.logs }
+    it "raises" do
+      expect{subject}.to raise_error NotImplementedError
+    end
+  end
+  describe "#execute!" do
+    subject { event.execute! }
+    it "raises" do
+      expect{subject}.to raise_error NotImplementedError
+    end
+  end
+  describe "#consume_time" do
+    subject { event.consume_time }
+    it "raises" do
+      expect{subject}.to raise_error NotImplementedError
+    end
+  end
+end
