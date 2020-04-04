91941126b5283646236661d102cc71e313f74dd8
jyllsarta <illstarred.dive@gmail.com>
2020-04-04 16:47:50 +0900

    annotate

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,14 +3,14 @@
 # Table name: items
 #
 #  id          :integer          not null, primary key
-#  agi         :integer
-#  base_rank   :integer
-#  def         :integer
-#  dex         :integer
+#  agi         :integer          default(0)
+#  base_rank   :integer          default(0)
+#  def         :integer          default(0)
+#  dex         :integer          default(0)
 #  flavor_text :string
 #  name        :string
-#  rarity      :integer
-#  str         :integer
+#  rarity      :integer          default(1)
+#  str         :integer          default(0)
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
