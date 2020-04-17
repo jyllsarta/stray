6e67e8e2ddecd192b1675edc49e0b63740482cc4
jyllsarta <illstarred.dive@gmail.com>
2020-04-18 00:56:10 +0900

    フィルターレイヤーは透明度を揺らす

--- app/javascript/Field.vue
+++ app/javascript/Field.vue
@@ -95,9 +95,11 @@ export default {
       }
     },
     viewStyle(viewId){
+      const opacity = viewId === 3 ? Math.abs(Math.sin(this.layerStatus[viewId] / 20)) * 0.7 + 0.3 : 1;
       return {
         backgroundImage: `url("/images/backgrounds/${this.$store.state.user.status.current_dungeon_id}/${viewId}.png")`,
         backgroundPosition: `-${this.layerStatus[viewId]}px 0`,
+        opacity: opacity,
       }
     },
   },
