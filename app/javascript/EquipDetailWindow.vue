89ebe756c091b4e0e774b5bc0a898101c2634727
jyllsarta <illstarred.dive@gmail.com>
2020-04-28 01:51:10 +0900

    アイテム詳細ウィンドウのスケッチ

--- app/javascript/EquipDetailWindow.vue
+++ app/javascript/EquipDetailWindow.vue
@@ -6,14 +6,91 @@
         .back_button.clickable(@click="$store.commit('window/updateWindowShowState', {windowName: 'equip_detail', state: false})")
         .title
           | 装備詳細
+      .current_coin
+        .icon
+          img(src="/images/ui/coin.png")
+        .amount
+          | {{$store.state.user.status.coin}}
       .description
-        | #########の詳細画面です。強化ができます。
+        | #########の詳細画面です。コインを消費して、所持しているアイテムの最大強化値(+77315)まで強化できます。
       .body
-        .current_coin
-          .icon
-            img(src="/images/ui/coin.png")
-          .amount
-            | {{$store.state.user.status.coin}}
+        .status_area
+          .before.parameter_box
+            .name
+              | ◆伝説のリズムカーニバル+765983
+            .total.item
+              .label
+                |  TOTAL
+              .value
+                | 123123123
+            .str.item
+              .label
+                |  STR
+              .value
+                | 123123123
+            .dex.item
+              .label
+                |  DEX
+              .value
+                | 123123123
+            .def.item
+              .label
+                |  DEF
+              .value
+                | 123123123
+            .agi.item
+              .label
+                |  AGI
+              .value
+                | 123123123
+          .after.parameter_box
+            .name
+              | ◆伝説のリズムカーニバル+765984
+            .total.item
+              .label
+                |  TOTAL
+              .value
+                | 123123123
+                .diff
+                  | (+82828282)
+            .str.item
+              .label
+                |  STR
+              .value
+                | 123123123
+                .diff
+                  | (+82828282)
+            .dex.item
+              .label
+                |  DEX
+              .value
+                | 123123123
+                .diff
+                  | (+82828282)
+            .def.item
+              .label
+                |  DEF
+              .value
+                | 123123123
+                .diff
+                  | (+82828282)
+            .agi.item
+              .label
+                |  AGI
+              .value
+                | 123123123
+                .diff
+                  | (+82828282)
+        .controls
+          .costs
+            .current
+              | 所持：123123123
+            .consumes
+              | 消費：3123123
+          .rank_up.clickable
+            | 強化
+        .enchantment_area
+          //ここは実装後に埋めればいいや
 
 
 </template>
@@ -49,20 +126,86 @@ export default {
   white-space: pre-wrap;
   border-bottom: 1px solid $gray3;
 }
+.current_coin{
+  position: absolute;
+  top: $space;
+  right: $space;
+  display: flex;
+  .icon{
+    img{
+      width: 32px;
+    }
+  }
+  .amount{
+    line-height: 32px;
+    font-size: $font-size-large;
+  }
+}
 .body{
   display: flex;
-  .current_coin{
+  flex-direction: column;
+  justify-content: space-around;
+  padding: $space;
+  width: 100%;
+  height: 440px;
+  .status_area{
+    height: 230px;
+    padding: $space;
     display: flex;
-    width: 30%;
-    .icon{
-      img{
-        width: 32px;
+    justify-content: center;
+    font-size: 18px; // ここは特異なフォントサイズにします。なんかどうしてもlargeだと大きくてnormalだと小さい...
+    .parameter_box{
+      padding: $space;
+      width: 400px;
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      .item{
+        width: 100%;
+        .label{
+          width: 45%;
+          padding-right: $space;
+          text-align: right;
+          display: inline-block;
+        }
+        .value{
+          width: 55%;
+          display: inline-block;
+          .diff{
+            display: inline-block;
+            padding-left: $space;
+          }
+        }
+      }
+      .name{
+        text-align: center;
       }
     }
-    .amount{
-      line-height: 32px;
-      font-size: $font-size-large;
+    .before{
+      border-right: 1px solid $gray3;
     }
+  }
+  .controls{
+    height: 60px;
+    padding: $space;
+    display: flex;
+    justify-content: center;
+    .costs{
+      width: 200px;
+    }
+    .rank_up{
+      width: 100px;
+      height: 100%;
+      line-height: 100%;
+      padding-top: 14px; // ここなぜか計算が合わないのでベタ指定にします
+      text-align: center;
+    }
+  }
+  .enchantment_area{
+    border-top: 1px solid $gray3;
+    height: 100px;
+    padding: $space;
   }
 }
 
