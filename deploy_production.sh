7bdc95f521a0eac3acb8db09a212b71b9b77b3b0
jyllsarta <illstarred.dive@gmail.com>
2020-05-06 01:11:22 +0900

    本番デプロイスクリプトを修正

--- deploy_production.sh
+++ deploy_production.sh
@@ -1,31 +1,31 @@
 #!/bin/zsh
 # for production deploy
 
-echo "# update repository"
+echo "##### update repository"
 git pull
 
-echo "# bundle install"
+echo "##### bundle install"
 bundle install
 
-echo "# yarn install"
+echo "##### yarn install"
 yarn install --check-files
 
-echo "# db migrate"
+echo "##### db migrate"
 RAILS_ENV=production bundle exec rails db:ridgepole:apply
 
-echo "# db seed"
+echo "##### db seed"
+cd masterdata/
+git pull
+cd ../
 RAILS_ENV=production bundle exec rails db:seed
 
-echo "# compile asset"
+echo "##### compile asset"
 yarn build
 
-echo "# kill older server"
-RAILS_ENV=production bundle exec rails db:seed
-
-echo "kill older server"
+echo "#### kill older server"
 PID=`ps ax | grep '[p]uma' | awk '{ print $1 }'`
 echo "older server is ${PID}"
 kill -9 ${PID}
 
-echo "start server"
+echo "##### start server"
 RAILS_ENV=production bundle exec rails s -d
