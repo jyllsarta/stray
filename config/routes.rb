966009e7eae9964766b184e91dd49487fd88136d
jyllsarta <illstarred.dive@gmail.com>
2020-05-12 22:33:46 +0900

    root to index を張る

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get root to: "clients#index"
   resources :clients, only: [:index]
   resources :users, only: [:create] do
     resources :user_items, only: [] do
