4e30f4bd0684716ffe06a13c094a3eca7678241b
jyll sarta <illstarred.dive@gmail.com>
2020-06-20 00:27:26 +0900

    title メタタグを環境によって出し分ける

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def webpack_target_path
     Rails.env.development? ? "http://localhost:3035/main.js" : "/packs/main.js"
   end
+
+  def meta_title
+    Rails.env.development? ? "<DEV> スピカとチロル" : "スピカとチロル"
+  end
 end
