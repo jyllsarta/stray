dc0a988eed259f0b6cc79d3415d259539aed722c
jyllsarta <illstarred.dive@gmail.com>
2020-08-26 00:03:56 +0900

    sentryに sourcemap を送信してみる

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,19 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
+const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 module.exports = merge(common, {
 　mode: 'production',
   performance: {
-    maxEntrypointSize: 500000,
-    maxAssetSize: 500000,
+    maxEntrypointSize: 1500000,
+    maxAssetSize: 1500000,
   },
+  plugins: [
+    new SentryWebpackPlugin({
+      include: ".",
+      ignoreFile: ".sentrycliignore",
+      ignore: ["node_modules", "webpack.config.js"],
+      configFile: "sentry.properties",
+    }),
+  ],
 })
