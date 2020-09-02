fdaaf16c147060876aed07f077c7396fdcb689e0
jyllsarta <illstarred.dive@gmail.com>
2020-09-02 19:30:45 +0900

    同一日にpublishされた場合、idの降順でさらにソートする

--- app/controllers/release_notes_controller.rb
+++ app/controllers/release_notes_controller.rb
@@ -1,5 +1,5 @@
 class ReleaseNotesController < ApplicationController
   def index
-    @release_notes = ReleaseNote.all.order(published_at: :desc)
+    @release_notes = ReleaseNote.all.order(published_at: :desc).order(id: :desc)
   end
 end
