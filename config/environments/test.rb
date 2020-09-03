ed43554822aa0ded4d234b4c608007b3ea9ecd49
jyllsarta <illstarred.dive@gmail.com>
2020-09-03 23:37:59 +0900

    redisキャッシュを導入

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
-  config.cache_store = :memory_store
+  config.cache_store = :redis_cache_store
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = true
