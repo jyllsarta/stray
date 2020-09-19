f9cfc01c96920c56a0ce163b79343e712168e304
jyll sarta <illstarred.dive@gmail.com>
2020-09-19 11:10:47 +0900

    バーが2pxに見えてしまうことの対策

--- app/javascript/EquipWindow.vue
+++ app/javascript/EquipWindow.vue
@@ -456,7 +456,7 @@ export default {
 
     .bar_area{
       width: 80%;
-      height: 1px;
+      height: 0.3px;
       display: flex;
       opacity: 0.9;
       .bar{
@@ -800,10 +800,10 @@ export default {
             }
             .bar_area{
               width: 100%;
-              height: 1px;
+              height: 0.3px;
               .bar{
                 width: 100%;
-                height: 1px;
+                height: 0.3px;
               }
             }
           }
@@ -818,11 +818,11 @@ export default {
               padding-left: $space;
             }
             .bar_area{
-              height: 1px;
+              height: 0.3px;
               width: 50%;
               .bar{
                 width: 100%;
-                height: 1px;
+                height: 0.3px;
               }
             }
           }
