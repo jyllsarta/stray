a8f4a33d01ba1abbbc691d6165342457797db823
jyllsarta <illstarred.dive@gmail.com>
2020-12-28 22:03:41 +0900

    プレイヤースキルカットインの清書

--- app/javascript/fragments/PlayerSkillCutin.vue
+++ app/javascript/fragments/PlayerSkillCutin.vue
@@ -1,8 +1,14 @@
 <template lang="pug">
 transition(name="anim")
-  .skill_cutin.topic_medium
-    .text
-      | プレイヤースキル！
+  .skill_cutin
+    .base
+      .head
+        | ファイア
+      .images
+        .background
+        .image
+      .descri
+        | 敵に5点ダメージ。
 </template>
 
 <script lang="ts">
@@ -26,16 +32,52 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import "../stylesheets/constants";
   .skill_cutin{
     position: absolute;
-    top: calc(40% - 25px);
-    left: calc(50% - 200px);
-    height: 50px;
-    width: 400px;
+    top: calc(25% - 75px);
+    left: calc(50% - 300px);
+    height: 150px;
+    width: 600px;
     opacity: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    border-top: 2px solid transparent;
+    border-bottom: 2px solid transparent;
+    .base{
+      width: 100%;
+      height: 100%;
+      .head{
+        width: 100%;
+        font-size: $font-size-large;
+        margin-bottom: $space;
+        text-align: center;
+      }
+      .images{
+        position: relative;
+        width: 100%;
+        height: 70%;
+        .background{
+          width: 100%;
+          height: 100%;
+          background: linear-gradient(to right, #805462 0%, $background 160%);
+        }
+        .image{
+          position: absolute;
+          bottom: $thin_space;
+          left: $space;
+          height: calc(100% - #{$thin_space} * 2);
+          width: 65%;
+          background-position: 30% 70%;
+          background-size: cover;
+          background-image: url("/images/ui/velocity/rank_2.png");
+        }
+      }
+      .descri{
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        font-style: italic;
+      }
+    }
   }
   .anim-leave-active {
     animation: anim 1.2s;
@@ -44,10 +86,18 @@ export default {
     0% {
       opacity: 0;
       transform: translateX(-10px);
+      background-color: transparent;
+      border-top: 2px solid transparent;
+      border-bottom: 2px solid transparent;
     }
     30% {
       opacity: 1;
       transform: translateX(0px);
+      background-color: #80546257;
+    }
+    50%{
+      border-top: 2px solid #d15e82;
+      border-bottom: 2px solid #d15e82;
     }
     70% {
       opacity: 1;
@@ -56,6 +106,10 @@ export default {
     100% {
       opacity: 0;
       transform: translateX(30px);
+      background-position-x: 0%;
+      background-color: transparent;
+      border-top: 2px solid transparent;
+      border-bottom: 2px solid transparent;
     }
   }
 </style>
