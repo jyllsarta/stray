d7879fa39ee6d442a45a52635a4a1e33c91d7b14
jyllsarta <illstarred.dive@gmail.com>
2021-01-31 18:12:51 +0900

    スキルのpreload漏れを解消

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,6 +1,6 @@
 class SkillsController < ApplicationController
   def index
-    @user_skills = current_user.skills.order(skill_id: :asc)
+    @user_skills = current_user.skills.preload(:skill).order(skill_id: :asc)
   end
 
   def equip
