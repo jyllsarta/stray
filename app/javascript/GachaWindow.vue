647dd526807e24780596afa63235a386810dab0b
jyllsarta <illstarred.dive@gmail.com>
2021-03-03 23:19:06 +0900

    WFの素組み

--- app/javascript/GachaWindow.vue
+++ app/javascript/GachaWindow.vue
@@ -40,9 +40,58 @@
               .rate
                 | 59.98%
         .coins
-          | XXXXXX,XXX
+          .current
+            | 99999999
+          .to.clickable
+            | ↓
+          .pool
+            | 123456
+          .go.clickable
+            | 投入！
         .rewards
-          | ご利益
+          .index
+            | - ご利益 -
+          .table
+            .reward
+              .total
+                | 999999999
+              img.icon(src="/images/ui/coin.png")
+              .cross
+                | x
+              .amount
+                | 9876543
+            .reward
+              .total
+                | 999999999
+              img.icon(src="/images/ui/coin.png")
+              .cross
+                | x
+              .amount
+                | 9876543
+            .reward
+              .total
+                | 999999999
+              img.icon(src="/images/ui/coin.png")
+              .cross
+                | x
+              .amount
+                | 9876543
+            .reward
+              .total
+                | 999999999
+              img.icon(src="/images/ui/coin.png")
+              .cross
+                | x
+              .amount
+                | 9876543
+            .reward
+              .total
+                | 999999999
+              img.icon(src="/images/ui/coin.png")
+              .cross
+                | x
+              .amount
+                | 9876543
         .pot
           img.pot_image(src="/images/gacha/pot1.png")
         .characters
@@ -125,8 +174,48 @@ export default {
       position: absolute;
       top: 100px;
       right: $space;
-      width: 200px;
-      height: 400px;
+      width: 400px;
+      height: 250px;
+      .index{
+        width: 100%;
+        text-align: center;
+        font-size: $font-size-large;
+      }
+      .table{
+        width: 100%;
+        height: 100%;
+        line-height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        align-items: flex-end;
+        .reward{
+          .total{
+            display: inline-block;
+            width: 6em;
+          }
+          .icon{
+            display: inline-block;
+            width: 20px;
+            height: 20px;
+          }
+          .cross{
+            display: inline-block;
+            width: 1em;
+            text-align: center;
+          }
+          .amount{
+            display: inline-block;
+            width: auto;
+            text-align: right;
+          }
+          @for $i from 1 through 10 {
+            &:nth-child(#{$i}) {
+              font-size:#{24 - $i}px;
+            }
+          }
+        }
+      }
     }
     .coins{
       position: absolute;
@@ -134,7 +223,20 @@ export default {
       $width: 400px;
       left: calc((100% - #{$width}) / 2);
       width: $width;
-      height: 150px;
+      height: 200px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      font-size: $font-size-large;
+      justify-content: space-between;
+      .current{}
+      .to{
+        padding: $space;
+      }
+      .pool{}
+      .go{
+        padding: $space;
+      }
     }
     .pot{
       position: absolute;
