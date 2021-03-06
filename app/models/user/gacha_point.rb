8f50cca2a4f052245788294219feb396570f05b6
jyll sarta <illstarred.dive@gmail.com>
2021-03-06 22:58:44 +0900

    ランダム報酬の実装

--- app/models/user/gacha_point.rb
+++ app/models/user/gacha_point.rb
@@ -18,10 +18,12 @@ class User::GachaPoint < ApplicationRecord
 
   def add!(amount)
     with_lock do
+      # TODO: 現在のポットグレードよりも高い額を入れようとしてたらエラー
       user.status.consume_coin!(amount)
       fixed_reward_messages = add_fixed_rewards!(amount)
+      random_reward_messages = add_random_rewards!(amount)
       self.increment!(:point, amount)
-      { fixed_rewards: fixed_reward_messages, random_rewards: [] }
+      { fixed_rewards: fixed_reward_messages, random_rewards: random_reward_messages }
     end
   end
 
@@ -31,5 +33,39 @@ class User::GachaPoint < ApplicationRecord
     # ポイント1000のユーザは1000の報酬はすでにもらってるので片方の境界を外す
     rewards = GachaFixedReward.where(point: (point+1)..(point+amount))
     rewards.map{ |reward| Gift.new(reward.giftable_type, reward.giftable_id, reward.amount).receive!(user) }
+  end
+
+  def add_random_rewards!(amount)
+    remain_point = amount
+    random_rewards = []
+    reward_count(point, amount, current_pot.reward_frequency).times do
+      # TODO: Gift経由じゃなくてランダム報酬は専用のロットロジックを使う、確率とかはそれを使う
+      # ...って考えてたけどめんどいし保守性も良くないので、ランク差分だけポットにもたせるかなぁ
+      random_rewards.push(Gift.new("RandomItem", 100, 1).receive!(user))
+    end
+    random_rewards
+  end
+
+  def current_pot
+    @_pot ||= Constants.gacha.variations.reverse.find do |variation|
+      point >= variation.min_point
+    end
+  end
+  
+  def reward_count(before, delta, freq)
+    # before:1100 + delta:2300 -> 4400 で、2000, 3000, 4000(freq: 1000)の報酬を得るシチュエーションで例える
+    # 1100 から次回報酬ライン 2000 をつくる
+    next_reward = (before + freq) - before % freq
+    # もし 1100 + X が 2000より少ないならなにもすることはない
+    return 0 if before + delta < next_reward
+    # 2000 よりは大きいので報酬はとりあえず1、それ以降どれくらい報酬があるか判定する
+    current = next_reward
+    count = 1
+    # 2000 + 1000 < 4400 報酬ラインを一つずつ超えるたびにカウント+1
+    while(current + freq <= before + delta)
+      count += 1
+      current += freq
+    end
+    count
   end
 end
--- spec/models/user/gacha_point_spec.rb
+++ spec/models/user/gacha_point_spec.rb
@@ -23,26 +23,68 @@ RSpec.describe User::GachaPoint, type: :model do
 
     let!(:user) { create(:user) }
     let!(:user_status) { create(:user_status, user: user) }
-    let!(:user_gacha_point){ create(:user_gacha_point, user: user) }
+    let!(:user_gacha_point){ create(:user_gacha_point, user: user, point: point) }
     let!(:gacha_fixed_reward){ create(:gacha_fixed_reward, point: 100, giftable_type: "Star", giftable_id: 1, amount: 5) }
     let!(:gacha_fixed_reward2){ create(:gacha_fixed_reward, point: 101, giftable_type: "Star", giftable_id: 1, amount: 5) }
     subject { user_gacha_point.add!(amount) }
 
     context "正常系" do
       let(:amount){ 100 }
+      let(:point){ 0 }
       before do
         user_status.update!(star: 0)
         user_status.add_coin!(amount)
       end
-      it "一通りの変換を行う" do
+      it "固定報酬の一通りの変換を行う" do
         subject
         expect(user.status.reload.coin).to eq(0)
         expect(user.status.reload.star).to eq(5)
         expect(user.gacha_point.reload.point).to eq(amount)
       end
+
+      context "ランダム報酬" do
+        before do
+          allow_any_instance_of(Gift).to receive_message_chain(:receive!).and_return("ギフトを受け取った！")
+        end
+
+        context "メッセージ" do
+          let(:amount){ 1000 }
+          let(:point){ 0 }
+          it "ランダムアイテムのギフトが積まれる" do
+            expect(subject).to match_json_expression(
+              fixed_rewards: Array,
+              random_rewards: ["ギフトを受け取った！"]
+            )
+          end
+        end
+        context "複数の報酬" do
+          # 1400 + 5800 = 7200 2000 ~ 7000 の 6報酬受取
+          let(:amount){ 5800 }
+          let(:point){ 1400 }
+          it "6" do
+            expect(subject[:random_rewards].length).to eq(6)
+          end
+        end
+        context "もらえないパターン" do
+          # 1299 + 700 = 1999 で　2000に足りない
+          let(:amount){ 700 }
+          let(:point){ 1299 }
+          it "0" do
+            expect(subject[:random_rewards].length).to eq(0)
+          end
+        end
+        context "ポットグレード違い" do
+          let(:amount){ 2100000 } # 最高鍋での報酬が２つもらえるライン
+          let(:point){ 10000000 } # 最高鍋
+          it "2" do
+            expect(subject[:random_rewards].length).to eq(2)
+          end
+        end
+      end
     end
     context "コイン不足" do
       let(:amount){ 100 }
+      let(:point){ 0 }
       before do
         user_status.add_coin(amount - 1)
       end
