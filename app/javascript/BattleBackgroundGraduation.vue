0ac5c1218ad57b5ccc847f0dc493a07401600eb3
jyll sarta <illstarred.dive@gmail.com>
2021-03-21 22:26:12 +0900

    FEに応じた背景グラデの色選び

--- app/javascript/BattleBackgroundGraduation.vue
+++ app/javascript/BattleBackgroundGraduation.vue
@@ -35,10 +35,25 @@ export default {
   height: 100%;
 
   &.bg_default{
-    background-image: linear-gradient(to bottom, $gray3 0%, transparent 10%);
+    background-image: linear-gradient(to bottom, #2c344e 0%, transparent 15%);
   }
   &.bg_102{
-    background-image: linear-gradient(to bottom, #37463d 0%, transparent 15%);
+    background-image: linear-gradient(to bottom, #1e3321 0%, transparent 15%);
+  }
+  &.bg_103{
+    background-image: linear-gradient(to bottom, #4e3430 0%, transparent 15%);
+  }
+  &.bg_104{
+    background-image: linear-gradient(to bottom, #454637 0%, transparent 15%);
+  }
+  &.bg_105{
+    background-image: linear-gradient(to bottom, #445a55 0%, transparent 15%);
+  }
+  &.bg_106{
+    background-image: linear-gradient(to bottom, #2b2350 0%, transparent 15%);
+  }
+  &.bg_107{
+    background-image: linear-gradient(to bottom, #44365f 0%, transparent 15%);
   }
 }
 </style>
