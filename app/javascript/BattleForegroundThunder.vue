6ee6f8eb4173b94492419da18921c0263e896185
jyll sarta <illstarred.dive@gmail.com>
2021-03-22 23:24:29 +0900

    雷のFE背景実装

--- app/javascript/BattleForegroundThunder.vue
+++ app/javascript/BattleForegroundThunder.vue
@@ -6,6 +6,9 @@
         :key="particle.id",
         :style="{left: particle.x, top: particle.y}"
       )
+        .cross_y
+        .cross_y2
+        .cross_y3
 
 </template>
 
@@ -24,20 +27,20 @@ export default {
   },
   store,
   mounted(){
-    setInterval(this.addParticle, 500);
+    setInterval(this.addParticle, 1200);
   },
   methods: {
     addParticle(){
       if(document.visibilityState !== "visible"){
         return;
       }
-      const maxParticleCount = 6;
+      const maxParticleCount = 4;
       if(this.particles.length > maxParticleCount){
         this.particles = this.particles.slice(-maxParticleCount)
       }
       const id = Math.floor(Math.random() * 999999999);
       const x = Math.floor(Math.random() * 700) + 162;
-      const y = Math.floor(Math.random() * 100) + 50;
+      const y = Math.floor(Math.random() * 100) + 150;
       this.particles.push({id: id, x: x, y: y})
     },
   }
@@ -49,23 +52,40 @@ export default {
 .particles{
   .particle{
     position: absolute;
-    width: 10px;
-    height: 10px;
-    background-color: #f3f56977;
     mix-blend-mode: screen;
+    .cross_y{
+      position: absolute;
+      width: 10px;
+      height: 10px;
+      background-color: #f3f569dc;
+      transform: scale(0.2, 4) rotate(45deg)
+    }
+    .cross_y2{
+      position: absolute;
+      width: 10px;
+      height: 10px;
+      background-color: #f3f56977;
+      transform: rotate(20deg) scale(0.2, 2.4) rotate(45deg)
+    }
+    .cross_y3{
+      position: absolute;
+      width: 10px;
+      height: 10px;
+      background-color: #f3f56977;
+      transform: rotate(-20deg) scale(0.2, 2.4) rotate(45deg)
+    }
   }
 }
 
 .particle-anim-enter-active, .particle-anim-leave-active {
-  transition: all 5s;
+  transition: all 2s;
 }
 .particle-anim-enter {
   opacity: 0;
-  transform: translateY(-100px) rotate(45deg) scale(1, 2) rotate(32deg);
+  transform: translateY(-300px);
 }
 .particle-anim-leave-to {
   opacity: 0;
-  transform: translateY(100px) rotate(-125deg) scale(1, 2) rotate(32deg);
 }
 
 </style>
