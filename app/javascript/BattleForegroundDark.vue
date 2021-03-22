429b489afb5e21724030ac2b5ca8a3637a0f510d
jyll sarta <illstarred.dive@gmail.com>
2021-03-23 00:05:28 +0900

    闇FE背景の実装

--- app/javascript/BattleForegroundDark.vue
+++ app/javascript/BattleForegroundDark.vue
@@ -24,20 +24,20 @@ export default {
   },
   store,
   mounted(){
-    setInterval(this.addParticle, 500);
+    setInterval(this.addParticle, 1000);
   },
   methods: {
     addParticle(){
       if(document.visibilityState !== "visible"){
         return;
       }
-      const maxParticleCount = 6;
+      const maxParticleCount = 3;
       if(this.particles.length > maxParticleCount){
         this.particles = this.particles.slice(-maxParticleCount)
       }
       const id = Math.floor(Math.random() * 999999999);
       const x = Math.floor(Math.random() * 700) + 162;
-      const y = Math.floor(Math.random() * 100) + 50;
+      const y = Math.floor(Math.random() * 50) + 160;
       this.particles.push({id: id, x: x, y: y})
     },
   }
@@ -49,23 +49,25 @@ export default {
 .particles{
   .particle{
     position: absolute;
-    width: 10px;
-    height: 10px;
-    background-color: #bd69f577;
+    width: 40px;
+    height: 40px;
+    background-color: #8d889e3d;
     mix-blend-mode: screen;
+    border-radius: 20px;
+    box-shadow: 0px 0px 10px 3px #504f79b7, 0px 0px 15px 2px #504f79b7 inset;
   }
 }
 
 .particle-anim-enter-active, .particle-anim-leave-active {
-  transition: all 5s;
+  transition: all 4s linear;
 }
 .particle-anim-enter {
-  opacity: 0;
-  transform: translateY(-100px) rotate(45deg) scale(1, 2) rotate(32deg);
+  opacity: 1;
+  transform: translateY(30px) scale(0);
 }
 .particle-anim-leave-to {
   opacity: 0;
-  transform: translateY(100px) rotate(-125deg) scale(1, 2) rotate(32deg);
+  transform: translateY(10px) scale(2);
 }
 
 </style>
