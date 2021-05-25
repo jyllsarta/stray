0abee23b438e65e604533d960c7dc8d436109749
jyll sarta <illstarred.dive@gmail.com>
2021-05-25 22:28:32 +0900

    デイリーエネミーはフィールドエフェクトを載せない

--- app/services/quest_battle.rb
+++ app/services/quest_battle.rb
@@ -52,7 +52,7 @@ class QuestBattle
         playerSkills: @user.skills.order(skill_id: :asc).equipped.map(&:skill).map(&:to_battle_skill),
         enemyCards: enemy_cards,
         enemySkills: @enemy.enemy_skills.map(&:skill).map(&:to_battle_skill),
-        fieldEffectStateId: @enemy.quest&.field_effect_state_id,
+        fieldEffectStateId: @is_daily ? nil : @enemy.quest&.field_effect_state_id,
     }.to_json
   end
 
