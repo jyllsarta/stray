2db1b3cd8076d1be8fdd258894848862fa24e468
jyll sarta <illstarred.dive@gmail.com>
2021-08-02 11:24:29 +0900

    サーバだけ起動するスクリプトを追加

---
+++ start_server.sh
@@ -0,0 +1,9 @@
+echo "#### kill older server"
+PID=`ps ax | grep '[p]uma' | awk '{ print $1 }'`
+if [ PID != "" ]; then
+  echo "older server is ${PID}"
+  kill -9 ${PID}
+fi
+
+echo "##### start server"
+RAILS_ENV=production bundle exec rails s
