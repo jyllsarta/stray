034ebd42621679a0f24bdc86475a2818ccc28a4b
jyll sarta <illstarred.dive@gmail.com>
2025-02-02 18:31:56 +0900

    sentry消えてくれ

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,5 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
-const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 module.exports = merge(common, {
 　mode: 'production',
@@ -9,11 +8,5 @@ module.exports = merge(common, {
     maxAssetSize: 1500000,
   },
   plugins: [
-    new SentryWebpackPlugin({
-      include: ".",
-      ignoreFile: ".sentrycliignore",
-      ignore: ["node_modules", "webpack.config.js"],
-      configFile: "sentry.properties",
-    }),
   ],
 })
